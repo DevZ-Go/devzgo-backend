97258e637f9e389adb31b63d3f5d26f87269ad52
Chan Gaganjeet Singh <sinchang@sheridancollege.ca>
2026-02-26 03:02:02 -0500

    Fix Alembic to use Docker DATABASE_URL

--- alembic/env.py
+++ alembic/env.py
@@ -61,15 +61,20 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
+        url=DATABASE_URL,  # <-- IMPORTANT
     )
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
         )
 
         with context.begin_transaction():
