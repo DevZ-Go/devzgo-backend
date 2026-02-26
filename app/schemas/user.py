c5503e44753dc149568bd749c023fa183929dbbd
Chan Gaganjeet Singh <sinchang@sheridancollege.ca>
2026-02-26 02:15:09 -0500

    Add user schemas

---
+++ app/schemas/user.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+from datetime import datetime
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: UUID
+    username: str
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
