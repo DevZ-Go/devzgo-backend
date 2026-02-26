33cc03275cd90f8265a2f88707b92a0c1b7930bb
Chan Gaganjeet Singh <sinchang@sheridancollege.ca>
2026-02-26 01:16:51 -0500

    Setup SQLAlchemy engine and Base Configuration

---
+++ app/db/session.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://devzgo:devzgo@localhost:5432/devzgo_db"
+)
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+# Importing the database base and engine to ensure that the database tables are created when the application starts
+from app.db.base import Base
+from app.db.session import engine
+
 app = FastAPI()
+
+# Create the database tables based on the models defined in the Base metadata
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
