9baaa2590f2f2a7894a064cc31ef39b7691efec7
Chan Gaganjeet Singh <sinchang@sheridancollege.ca>
2026-02-26 02:38:54 -0500

    Fix missing get_db dependency

--- app/main.py
+++ app/main.py
@@ -3,14 +3,14 @@ from fastapi import FastAPI
 # Importing the database base and engine to ensure that the database tables are created when the application starts
 from app.db.base import Base
 from app.db.session import engine
-from app.routes import auth
+from app.routes.auth import router as auth_router
 
 app = FastAPI()
 
 # Create the database tables based on the models defined in the Base metadata
 # Base.metadata.create_all(bind=engine)
 
-app.include_router(auth.router)
+app.include_router(auth_router)
 
 @app.get("/")
 def root():
